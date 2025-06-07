ddd53db6016201de7dfd3eca9bb2f0c43a6ffe1f
Andrew Saienko <andrew.saienko.308@gmail.com>
2025-06-07 12:40:48 +0300

    initial commit

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+declare module "@deimos308/math-lib" {
+  function sum(a: number, b: number): number;
+  function minus(a: number, b: number): number;
+  function divide(a: number, b: number): number;
+  function multiple(a: number, b: number): number;
+}
